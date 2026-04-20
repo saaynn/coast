a7b3e221b70f5957f2f98f8cde95d40040826d0a
saaynn <connect.saaynn@gmail.com>
2026-04-20 21:32:27 +0530

    Create build.sh

---
+++ coast/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Download the static build of FFmpeg for Linux
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+
+# Extract the archive
+tar -xf ffmpeg-release-amd64-static.tar.xz
+
+# Move the ffmpeg and ffprobe binaries to the root directory
+mv ffmpeg-*-static/ffmpeg .
+mv ffmpeg-*-static/ffprobe .
+
+# Clean up the downloaded archive files
+rm -rf ffmpeg-*-static*
