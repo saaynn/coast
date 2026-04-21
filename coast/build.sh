17bfc4538f38c45d88c509127af9e54967ae53b4
saaynn <connect.saaynn@gmail.com>
2026-04-21 11:54:19 +0530

    Update build.sh

--- coast/build.sh
+++ coast/build.sh
@@ -3,6 +3,8 @@
 # Install Python dependencies
 pip install -r requirements.txt
 
+pip install --upgrade yt-dlp
+
 # Download the static build of FFmpeg for Linux
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 
